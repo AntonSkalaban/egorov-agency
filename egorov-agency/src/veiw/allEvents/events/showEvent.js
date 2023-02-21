d606f862214cc3b21e0897524837172840772fb0
AntonSkalaban <112729189+AntonSkalaban@users.noreply.github.com>
2023-02-21 21:17:40 +0300

    fix: fix slider animation

--- egorov-agency/src/veiw/allEvents/events/showEvent.js
+++ egorov-agency/src/veiw/allEvents/events/showEvent.js
@@ -9,8 +9,8 @@ const showEvent = (e) => {
     if (eventsBody[i].classList.contains('event__body_active')) {
       eventsBody[i].classList.remove('event__body_active');
       eventsBody[newKey.dataset.id].classList.add('event__body_active');
-      eventsKeys[i].style.background = `linear-gradient(180deg, rgba(22, 44, 78, 0) 0%, #162C4E 100%), ${cardsBody[i].style.backgroundImage}`;
       newKey.style.background = '#162C4E';
+      eventsKeys[i].style.background = `linear-gradient(180deg, rgba(22, 44, 78, 0) 0%, #162C4E 100%), ${eventsBody[i].style.backgroundImage}`;
       break;
     }
   
